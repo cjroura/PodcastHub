b4e6e873f602e371f039ce51123c38fe88862aac
Christopher J. Roura <Christopher@Roura.Tech>
2024-09-09 17:27:29 -0400

    Initial Commit

---
+++ PodcastHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PodcastHub
+//
+//  Created by Christopher J. Roura on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PodcastHub/PodcastHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastHubApp.swift
+//  PodcastHub
+//
+//  Created by Christopher J. Roura on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PodcastHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
